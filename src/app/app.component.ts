7963852461402d7014591e5903c58bea30e98ff7
Swapnil Patil <patilswapnil230@ymail.com>
2021-01-12 20:37:34 +0530

    6 comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,9 +31,11 @@ export class AppComponent implements OnInit {
             if (number > 150 || window.pageYOffset > 150) {
                 // add logic
                 navbar.classList.remove('navbar-transparent');
+                navbar.classList.add('navbar-hidden');
             } else {
                 // remove logic
                 navbar.classList.add('navbar-transparent');
+                navbar.classList.remove('navbar-hidden');
             }
         });
         var ua = window.navigator.userAgent;
